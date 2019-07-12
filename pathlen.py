65b216c526551077b76afc5552b1fb2fcf89f530
genyez <genyez@163.com>
2019-07-12 14:13:54 +0800

    first

---
+++ pathlen.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import string
+
+maxlen = 255
+def getAllDrv():
+    drvs = []
+    for c in string.ascii_uppercase:
+        path = c + ":"
+        if os.path.isdir(path):
+            drvs.append(path)
+
+    return drvs
+
+def work():
+    outlist = []
+    filecount = 0
+    for drv in getAllDrv():
+        print("start searching drive %s" % drv)
+        for root ,dirs, files in os.walk(drv):
+            for file in files:
+                filecount += 1
+                print("searched file: %d" % filecount)
+                fullpath = os.path.join(os.path.abspath(root), file)
+                if len(fullpath) > maxlen:
+                    outlist.append(fullpath)
+    return outlist
+
+def main():
+    global maxlen
+    if len(sys.argv) > 1:
+        try:
+            maxlen = int(sys.argv[1])
+        except:
+            pass
+    outlist = work()
+    with open("result.txt", "wt") as f:
+        f.write("\n".join(outlist))
+
+if __name__ == '__main__':
+    main()
